10a57e92239f2c9cf32e01c6d58dd520fc22c636
James Murphy <murphyjj@my.gvltec.edu>
2021-02-26 21:43:52 -0600

    adding testing

---
+++ test/app.test.js
@@ -0,0 +1,25 @@
+const supertest = require("supertest");
+const app = require("../app");
+const { expect } = require("chai");
+
+describe("GET /apps", () => {
+  it("should return an array of apps", () => {
+    return supertest(app)
+      .get("/apps")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("array");
+        expect(res.body).to.have.lengthOf.at.least(1);
+        const app = res.body[0];
+        expect(app).to.include.all.keys("Reviews", "Size", "Installs");
+      });
+  });
+
+  it("should be 400 if sort is incorrect", () => {
+    return supertest(app)
+      .get("/apps")
+      .query({ sort: "MISTSKE" })
+      .expect(400, "Sort must be one of Rating or App");
+  });
+});
